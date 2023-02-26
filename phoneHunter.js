9455f7d30019f90b0dcfa9a8de0365380566cf60
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-02-26 20:22:00 +0600

    finally done

--- phoneHunter.js
+++ phoneHunter.js
@@ -1,15 +1,24 @@
-const loadPhone = async (searchText)=>{
+const loadPhone = async (searchText,dataLimit)=>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res =await fetch(url)
     const data = await res.json();
-    displayPhones(data.data)
+ 
+    displayPhones(data.data,dataLimit)
 }
 
-const displayPhones = phones =>{
+const displayPhones =( phones,dataLimit) =>{
      const phonesContainer  = document.getElementById('phone-container');
      phonesContainer.textContent='';
     //  display 20 phones only
-     phones =phones.slice(0,6)
+    const showAll =document.getElementById('show-all')
+   if(dataLimit && phones.length>10){
+    phones = phones.slice(0,10)
+    showAll.classList.remove('d-none')
+
+   }else{
+    showAll.classList.add('d-none')
+   }
+     
     // display No phone found
 
      const noPhnFound = document.getElementById('no-phn-found')
@@ -31,6 +40,11 @@ const displayPhones = phones =>{
           <div class="card-body">
             <h5 class="card-title">${phones.phone_name}</h5>
             <p class="card-text"> ${phones.slug}</p>
+          
+            <!-- Button trigger modal -->
+            <button onclick="loadDetails('${phones.slug}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Details
+            </button>
           </div>
         </div>
       
@@ -46,13 +60,40 @@ const displayPhones = phones =>{
     toggleSpinner(false)
 }
 
-document.getElementById('btn-search').addEventListener('click',function(){
-    // start loader
+const processing =(dataLimit)=>{
     toggleSpinner(true)
     const searchField = document.getElementById('search-field');
      const searchText = searchField.value;
-     loadPhone(searchText)
+     loadPhone(searchText,dataLimit)
+}
+
+
+document.getElementById('btn-search').addEventListener('click',function(){
+    // start loader
+    processing(10)
 })
+
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    processing()
+})
+
+
+// handle enter event handler
+document.getElementById('search-field').addEventListener('keydown', function (e) {
+
+    console.log(e.key)
+    if (e.key === 'Enter') {
+        processing(10)
+    }
+});
+
+
+
+
+
+
+
+
 
 const toggleSpinner =isLoading =>{
     const loaderSection = document.getElementById('loader')
@@ -63,4 +104,31 @@ const toggleSpinner =isLoading =>{
     }
 }
 
+
+// load details 
+const loadDetails =async id =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneData (data.data);
+
+
+}
+const displayPhoneData =(phone)=>{
+  console.log(phone)
+const modalTitle = document.getElementById('#exampleModal');
+  modalTitle.innerText=phone.name;
+  const phoneDetails =document.getElementById('phone-details');
+  phoneDetails.innerHTML=`
+  
+  <p>Release Date : ${phone.releaseDate ? phone.releaseDate : 'No ReleaseDate found'} </p>
+  <p>Release Date : ${phone.mainFeatures ? phone.mainFeatures.storage : 'No ReleaseDate found'} </p>
+  <p>Release Date : ${phone.mainFeatures ? phone.mainFeatures.chipSet: 'No ReleaseDate found'} </p>
+  <p>Release Date : ${phone.mainFeatures ? phone.mainFeatures.displaySize: 'No ReleaseDate found'} </p>
+  <p>Release Date : ${phone.mainFeatures ? phone.mainFeatures.memory : 'No ReleaseDate found'} </p>
+  <p> others : ${phone.others ? phone.others.Bluetooth : 'No ReleaseDate found'} </p>
+  <p> Brand : ${phone.brand ? phone.brand : 'No ReleaseDate found'} </p>
+   
+  `
+}
 //loadPhone()
